cb554e21766f3f044a18e04344b1a2911555ae10
Robin Ödling <robin.odling@codemill.se>
2025-08-14 14:35:10 +0200

    fix(av_lta): allow create or update roles to fail without breaking system

--- av_lta/apps.py
+++ av_lta/apps.py
@@ -1,4 +1,11 @@
+import logging
+
 from django.apps import AppConfig
+from django.db.utils import ProgrammingError
+
+from portal.pluginbase.core import PluginError
+
+log = logging.getLogger(__name__)
 
 
 class AVLTAConfig(AppConfig):
@@ -8,4 +15,7 @@ class AVLTAConfig(AppConfig):
     def ready(self):
         from .permissions import create_or_update_roles
 
-        create_or_update_roles()
+        try:
+            create_or_update_roles()
+        except Exception:
+            log.exception("AVLTA Plugin: Could not create roles")
